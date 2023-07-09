567fe15d845c4d3d18e115635921f02b4aae589e
Shahd Yousef <shahdyousefak@Shahds-MacBook-Pro.local>
2023-07-09 19:43:13 -0400

    pendulums are displaying, although incorrectly, with styling added

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ function checkForEmptyParameters(arr1, arr2, arr3, arr4, arr5) {
 
   if(arrays.some((arr) => arr.some((element) => element === ''))){
         // Display an error message for incomplete fields
-        errorMessage.textContent = 'Please fill out all fields';
+        errorMessage.textContent = 'Please fill out all fields - numbers only.';
         errorMessage.style.display = 'block';
         return 1; 
   } else return 0;
@@ -29,14 +29,14 @@ function checkForInvalidParameters(arr1, arr2, arr3, arr4, arr5) {
   const masses = [arr1[1], arr2[1], arr3[1], arr4[1], arr5[1]];
   const angles = [arr1[2], arr2[2], arr3[2], arr4[2], arr5[2]];
 
-  lenInvalid = lengths.some(element => parseFloat(element) > 200) ? 1 : 0;
+  lenInvalid = lengths.some(element => parseFloat(element) > 2000) ? 1 : 0;
   massInvalid = masses.some(element => parseFloat(element) > 500) ? 1 : 0;
   angInvalid = angles.some(element => parseFloat(element) > 180) ? 1 : 0;
 
   const errorMessages = [];
 
   if (lenInvalid) {
-    errorMessages.push('Length cannot be more than 200 cm.');
+    errorMessages.push('Length cannot be more than 2000 cm.');
   }
   if (massInvalid) {
     errorMessages.push('Mass cannot be more than 500 g.');
@@ -64,8 +64,7 @@ function startPendulum(event) {
   const errorMessage = document.getElementById('error-message'); 
   errorMessage.textContent = ''; // Remove error message
 
-  createPendulums(pendulumValues);
-
+  console.log("Sending data to the api..");
   fetch('/api/pendulums', {
     method: 'POST',
     headers: {
@@ -82,7 +81,8 @@ function startPendulum(event) {
   .then(response => response.json())
   .then(data => {
     console.log(data);
-    // Add your logic here to handle the response from the server
+    console.log("Drawing pendulums...");
+    drawPendulums(); // Call drawPendulums function after receiving the response
   })
   .catch(error => {
     console.error(error);
@@ -118,12 +118,68 @@ function togglePendulum(event) {
     pauseButton.innerText = 'Pause';
     pauseButton.style.display = 'inline-block';
     startPendulum(event); // Call startPendulum function
+
   } else {
     // stop button is clicked
     startButton.innerText = 'Start';
     pauseButton.style.display = 'none';
   }
 }
+
+function drawPendulums() {
+  const canvas = document.getElementById('pendulumCanvas');
+  const ctx = canvas.getContext('2d');
+  // Clear the canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Draw the rigid support (rod)
+  ctx.beginPath();
+  ctx.moveTo(0, canvas.height / 2);
+  ctx.lineTo(canvas.width, canvas.height / 2);
+  ctx.strokeStyle = 'black';
+  ctx.lineWidth = 4;
+  ctx.stroke();
+
+  const pendulumPositions = [
+    { x: canvas.width / 2, y: canvas.height / 2, color: 'hotpink' }, // Pendulum 1
+    { x: canvas.width / 2, y: canvas.height / 2, color: 'seagreen' }, // Pendulum 2
+    { x: canvas.width / 2, y: canvas.height / 2, color: 'deepskyblue' }, // Pendulum 3
+    { x: canvas.width / 2, y: canvas.height / 2, color: 'rebeccapurple' }, // Pendulum 4
+    { x: canvas.width / 2, y: canvas.height / 2, color: 'gold' } // Pendulum 5
+  ];
+
+  const scalingFactor = 0.2; // Adjust the scaling factor based on your requirements
+
+  // Draw the pendulum rods and balls
+  for (let i = 0; i < pendulumPositions.length; i++) {
+    const pendulumPos = pendulumPositions[i];
+    const length = parseFloat(getPendulumValues()[i][0]);
+    const mass = parseFloat(getPendulumValues()[i][1]);
+    const angle = parseFloat(getPendulumValues()[i][2]);
+
+    const rodLength = length * scalingFactor; // Calculate the rod length based on the length input and scaling factor
+    const ballRadius = mass * scalingFactor; // Calculate the ball radius based on the mass input and scaling factor
+
+    // Calculate the position of the pendulum bob (ball)
+    const bobX = pendulumPos.x + rodLength * Math.sin(angle);
+    const bobY = pendulumPos.y + rodLength * Math.cos(angle);
+
+    // Draw the pendulum rod
+    ctx.beginPath();
+    ctx.moveTo(pendulumPos.x, pendulumPos.y);
+    ctx.lineTo(bobX, bobY);
+    ctx.strokeStyle = pendulumPos.color;
+    ctx.lineWidth = 2;
+    ctx.stroke();
+
+    // Draw the pendulum ball
+    ctx.beginPath();
+    ctx.arc(bobX, bobY, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = pendulumPos.color;
+    ctx.fill();
+  }
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
 
